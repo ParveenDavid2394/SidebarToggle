8e6dd8d0cbd25971620b42140c0c9b00a14d8dd8
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-12 12:21:45 +0800

    gave toggle and close button features

---
+++ app.js
@@ -0,0 +1,13 @@
+// get items
+
+const toggleButton = document.querySelector('.sidebar-toggle');
+const closeButton = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleButton.addEventListener('click', ()=>{
+    sidebar.classList.toggle('show-sidebar');
+})
+
+closeButton.addEventListener('click', () => {
+    sidebar.classList.remove('show-sidebar');
+})
